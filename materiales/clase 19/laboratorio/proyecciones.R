155930f929df55862c93ac84638a763698d75ab5
BrunoGoat <brunopintosabelo@gmail.com>
2024-11-21 16:49:08 -0300

    Lab casi resuelto

--- materiales/clase 19/laboratorio/proyecciones.R
+++ materiales/clase 19/laboratorio/proyecciones.R
@@ -13,42 +13,58 @@
 # Vamos a trabajar con una población cerrrada y compuesta sólo por mujeres.
 
 # Cargamos los datos
-sw <- 
+
+sw <- read.table(file.path("datos","sweden_data_1993.txt"),
+                 header=F, stringsAsFactors = F)
 
 # Agregamos nombres a las columnas: "age", "p93", "L", "f"
 
+names(sw) <- c("age", "p93", "L", "f")
 
 # Ejercicio: Describir el contenido de cada columna de la tabla
 
+# La primer columna tiene los limites superiores (no incluidos) de los intervalos de la población
+# La segunda columna nos dice la cantidad de poblacion que se encuentra entre el limite inferior y superior de la edad
+# La tercera columna representan los años persona de las pesonas que pertenece al intervalo
+# La cuarta son las tasas de fecundidad por intervalo
+
+
 # definimos el sex ratio at birth
-srb <- 1.05
+srb <- 1.05 
 
 # Salvamos las columnas relevantes en objetos para facilitar la lectura del código
 L <- sw$L
 f <- sw$f
 N <- sw$p93
 
+options(scipen = 990)
 
 # guardamos el largo de L en un objeto "n"
-
+n <- length(L)
 
 # calcular las probabilidades de supervivencia en cada intervalo en un vector "px"
-px <- 
-  
-# agregar la probabilidad en el intervalos abierto (últimos dos intervalos)
-px[n-1] <- px[n] <- 
+px <- L[2:n]/L[-n]
 
-  
+
+# agregar la probabilidad en el intervalos abierto (últimos dos intervalos)
+
+t85 <- L[n]
+t80 <- L[n-1] + t85
+px[n-1] <- px[n] <- t85/t80
+
+
 # Calculamos un vector auxiliar con la población en 1998
   
 p98_aux <- N * px  
 
 # calcular la población mayor de 5 en 1998 (usand la información en p98_aux)
+
+
 sw$p98 <- NA
-sw$p98[2:(n-1)] <- 
-sw$p98[n] <- 
+sw$p98[2:(n-1)] <- p98_aux[1:(n-2)]
+sw$p98[n] <- sum(p98_aux[(n-1):n])
   
-  
+
 # definimos un vector vacio "B" para guardar en los próximos pasos los nacimientos
 B <- vector()  
   
@@ -56,7 +72,7 @@ B <- vector()
 idf <- which(f>0)
 
 for(i in idf){
-  B[i-min(idf-1)] <- #completar
+  B[i-min(idf-1)] <- f[i] * 5 * ((N[i] + N[i-1]*px[i])/2) #completar
 }
   
   
